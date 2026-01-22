b6258df14cf383a4837c60f2086bdcf67f592ea2
praveenkumar <praveenkumar.tst@gmail.com>
2026-01-21 23:13:58 -0600

    conversation and varaibles created

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Praveen :: hey Chinna, Hoe are you?"
+echo "Chinna :: I am doing. How about you?"
+echo "Praveen :: i am fine. Thanks! what's up"
+echo "Chinna :: nothing. Just going for walking. Do you want to join with me"
+echo "Praveen :: Not this time. I have some other work. Will join tomorrow"
