a46755506e8b5447b5ba0b0f837a69f9a5bd1074
praveenkumar <praveenkumar.tst@gmail.com>
2026-01-21 23:11:34 -0600

    conversation and varaibles created

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=Praveen
+PERSON2=Chinna
+
+echo "$PERSON1 :: hey $PERSON2, Hoe are you?"
+echo "$PERSON2 :: I am doing. How about you?"
+echo "$PERSON1 :: i am fine. Thanks! what's up"
+echo "$PERSON2 :: nothing. Just going for walking. Do you want to join with me"
+echo "$PERSON1 :: Not this time. I have some other work. Will join tomorrow"
