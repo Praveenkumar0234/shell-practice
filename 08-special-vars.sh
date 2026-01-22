841f241d522c295ded7e22521a25dc87be688f46
praveenkumar <praveenkumar.tst@gmail.com>
2026-01-22 14:53:03 -0600

    arrays created

---
+++ 08-special-vars.sh
@@ -0,0 +1,13 @@
+#!/bin?bash
+
+echo "All varaiables paased to the script: $@"
+echo "number of varaiables in the script: $#"
+echo "script name: $0"
+echo "current directory: $PWD"
+echo "user running the script: $USER"
+echo "home directory of the user: $HOME"
+echo "PID of the script: $$"
+sleep 10 &
+echo "PID of the last command running on the background: $!"
+
+
