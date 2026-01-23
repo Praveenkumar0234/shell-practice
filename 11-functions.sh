465ae5eea1431c1d45836ea41ecb2ae9089ee133
praveenkumar <praveenkumar.tst@gmail.com>
2026-01-22 23:23:02 -0600

    functions

--- 11-functions.sh
+++ 11-functions.sh
@@ -10,3 +10,19 @@ else
     echo "user is running with root acess"
 fi
 
+dnf list installed mysql
+
+if [ $? -ne 0]
+then 
+    echo "mysql is not installed...going to install it"
+    dnf install mysql -y
+    if [ $? -eq 0]
+    then  
+        echo "mysql installation is sucessful"
+    else
+        echo "ERROR:: mysql istallation is not successful"
+        exit 1
+else
+    echo "mysql has already installed....nothing to do"
+fi
+
