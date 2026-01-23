14f25f239e759203231cfa1542fd6f3e161369e6
praveenkumar <praveenkumar.tst@gmail.com>
2026-01-22 23:30:54 -0600

    functions

--- 11-functions.sh
+++ 11-functions.sh
@@ -10,20 +10,48 @@ else
     echo "user is running with root acess"
 fi
 
+VALIDATE () {
+if [ $! -eq 0 ]
+    then  
+        echo "$2 installation is sucessful"
+    else
+        echo "ERROR:: $2 istallation is not successful"
+        exit 1
+    fi
+}
+
 dnf list installed mysql
 
 if [ $? -ne 0 ]
 then 
     echo "mysql is not installed...going to install it"
     dnf install mysql -y
-    if [ $? -eq 0 ]
-    then  
-        echo "mysql installation is sucessful"
-    else
-        echo "ERROR:: mysql istallation is not successful"
-        exit 1
-    fi
+    validate $1 "mysql"
 else
     echo "mysql has already installed....nothing to do"
+fi
+
+
+dnf list installed nginx
+
+if [ $? -ne 0 ]
+then 
+    echo "nginx is not installed...going to install it"
+    dnf install nginx -y
+    validate $1 "nginx"
+else
+    echo "nginx has already installed....nothing to do"
+fi
+
+
+dnf list installed python3
+
+if [ $? -ne 0 ]
+then 
+    echo "python is not installed...going to install it"
+    dnf install python3 -y
+    validate $1 "python"
+else
+    echo "python has already installed....nothing to do"
 fi
 
